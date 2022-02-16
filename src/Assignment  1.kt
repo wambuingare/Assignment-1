b25cd7df9b64ae581eb6631ce76fe0cc990c7f5d
wambuingare <wambuinancyngare@gmail.com>
2022-02-16 21:59:43 +0300

    my first assignment

---
+++ src/Assignment  1.kt
@@ -0,0 +1,14 @@
+fun main() {
+ var name: String=("Nancy Wambui")
+ var age: Byte=(21)
+ var phonenumber =("0725124209")
+ var weight_in_kgs =(53)
+ var Kenyan: Boolean=true
+
+ println(name)
+ println(age)
+ println(phonenumber)
+ println(weight_in_kgs)
+ println(Kenyan)
+
+}
